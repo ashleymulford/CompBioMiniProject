f5158d38c5b7bcab9f4cfff11eec3da2a742a695
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-02-20 11:36:08 -0600

    Update 02_generate_input.py

--- 02_generate_input.py
+++ 02_generate_input.py
@@ -3,6 +3,8 @@ from Bio.Seq import Seq
 from Bio import SeqIO
 
 Entrez.email = "amulford@luc.edu"
-handle = Entrez.efetch(db="nucleotide", id="EF999921", rettype="fasta")
-records = list(SeqIO.parse(handle, "fasta"))
-print(records)
+handle = Entrez.efetch(db="nucleotide", id="EF999921", rettype="gb")
+records = list(SeqIO.parse(handle, "gb"))
+rec = records[0]
+
+print(rec)
