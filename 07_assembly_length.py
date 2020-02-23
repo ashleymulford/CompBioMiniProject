3192cc7f40eecb215e263ca1bf8af6255e1fe139
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-02-23 16:24:17 -0600

    Update 07_assembly_length.py

--- 07_assembly_length.py
+++ 07_assembly_length.py
@@ -1,7 +1,7 @@
-#import biopython classes
 from Bio.Seq import Seq
 from Bio import SeqIO
 
+#open files
 file = open("contigs_1000.fasta")
 log_file = open("miniProject.log", "a")
 
@@ -9,9 +9,11 @@ total_bps = 0
 
 #pull seqs out of file
 for contig in SeqIO.parse(file, "fasta"):
+  #get length and add to total bps
   length = len(contig.seq)
   total_bps = total_bps + length
-    
+
+#update log file
 log_file.write("There are " + str(total_bps) + " bp in the assembly.\n")
 log_file.close()
   
