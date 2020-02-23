35908a3f17c8c9debb13dde77076a12926dc4fa6
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-02-23 16:27:17 -0600

    Update 08_concat_contigs.py

--- 08_concat_contigs.py
+++ 08_concat_contigs.py
@@ -1,19 +1,20 @@
-#import biopython classes
 from Bio.Seq import Seq
 from Bio import SeqIO
 
+#open files
 file = open("contigs_1000.fasta")
 output_file = open("blast_input.fasta", "w")
   
 assembly = ""
-
 n=""
 
+#make string of 50 N to separate contigs
 for i in range(50):
   n = n+"N"
   
 #pull seqs out of file
 for contig in SeqIO.parse(file, "fasta"):
+  #create assembly
   assembly = assembly + str(contig.seq) + n
     
 output_file.write(">assembly\n")
