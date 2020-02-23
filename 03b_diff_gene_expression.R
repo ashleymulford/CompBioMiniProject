193bd8a05bdae4c81a69dc3591d50fbf840b1728
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-02-23 16:18:12 -0600

    Update 03b_diff_gene_expression.R

--- 03b_diff_gene_expression.R
+++ 03b_diff_gene_expression.R
@@ -1,24 +1,30 @@
+#import libraries
 library(sleuth)
 library(dplyr)
 
-sample_id <- dir(file.path("/home/amulford/results"))
-kal_dirs <- file.path("/home/amulford/results", sample_id)
-condition<- c("2 days post-infection", "6 days post-infection", "2 days post-infection", "6 days post-infection")
-table1<-as.data.frame(cbind(sample_id, condition, kal_dirs))
+sample_id <- dir(file.path("/home/amulford/results")) #list of srrs 
+kal_dirs <- file.path("/home/amulford/results", sample_id) #list of paths
+condition<- c("2 days post-infection", "6 days post-infection", "2 days post-infection", "6 days post-infection") #list of conditions
+table1<-as.data.frame(cbind(sample_id, condition, kal_dirs)) #make data frame
 
-colnames(table1)<-c("sample", "condition", "path")
-table1$sample<-as.character(table1$sample)
+colnames(table1)<-c("sample", "condition", "path") #add column names
+table1$sample<-as.character(table1$sample) #convert to char from factor
 table1$condition<-as.character(table1$condition)
 table1$path<-as.character(table1$path)
 
+#prep data
 s_prep <- sleuth_prep(table1, extra_bootstrap_summary = TRUE)
 
+#fit data
 s_fit <- sleuth_fit(s_prep, ~condition, 'full')
 s_fit <- sleuth_fit(s_fit, ~1, 'reduced')
+#run test
 s_fit <- sleuth_lrt(s_fit, 'reduced', 'full')
 
+#output results
 sleuth_table <- sleuth_results(s_fit, 'reduced:full', 'lrt', show_all = FALSE)
-sleuth_significant <- dplyr::filter(sleuth_table, qval <= 0.05)
-save_sleuth_significant<-sleuth_significant[1:4]
+sleuth_significant <- dplyr::filter(sleuth_table, qval <= 0.05) #filter for signifcance
+save_sleuth_significant<-sleuth_significant[1:4] #extract first four columns
 
+#output to log
 write.table(save_sleuth_significant, "miniProject.log", append = TRUE, sep = "\t", col.names = TRUE, quote = FALSE, row.names = FALSE)
